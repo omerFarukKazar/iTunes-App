7617452feeaa4f0b8913df01f91b0d78c42a7a6a
omerfkzr <omer.kazar22@gmail.com>
2022-12-16 15:20:51 +0300

    Basic Project Settings have been done. Project Folder Hierarchy changed. Design Pattern is changed to MVC. Settings for Programmatic UI have been done.

--- iTunes App/iTunes App/Screens/Main/Controller/MainViewController.swift
+++ iTunes App/iTunes App/Screens/Main/Controller/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  iTunes App
 //
 //  Created by Ömer Faruk Kazar on 16.12.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
